0a23677d00df87dd8d053173b577a4c4f911861d
anantakhanal16 <anantaramankhanal@gmail.com>
2024-12-30 13:05:40 +0545

    Initial commit

---
+++ src/PubSubSns/PubSubSnsStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace PubSubSns
+{
+    public class PubSubSnsStack : Stack
+    {
+        internal PubSubSnsStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
